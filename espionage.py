92738c660bef293b4a277f63b0d0fde564e85b37
Josh Schiavone (josh0xA) <jshschiavone@gmail.com>
2020-07-05 17:32:53 -0400

    better verbose and normal handling techniques

--- espionage.py
+++ espionage.py
@@ -147,50 +147,50 @@ def espionage_main():
 
                 print(BOLD + G + "[espionage]>" + W + BOLD + 'Ethernet Frame: ')
                 esp.print_espionage_notab('Destination: {}, Source: {}, Protocol: {}\n'.format(dest_mac, src_mac, eth_proto))
+                try:
+                    (packet_version, packet_header_length, packet_ttl, packet_protocol, packet_source, packet_destination, pkdata) = pk.handle_ipv4_packet(data)
+                    if eth_proto == 8:
+                        opt.__write_ipv4_normal_output__(data)
 
-                (packet_version, packet_header_length, packet_ttl, packet_protocol, packet_source, packet_destination, pkdata) = pk.handle_ipv4_packet(data)
-                if eth_proto == 8:
-                    opt.__write_ipv4_normal_output__(data)
-
-                    if args.filename:
-                        PCAP(pcap_file_name).write_to_pcap_file("\nIPv4 Packet Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
-                        PCAP(pcap_file_name).write_to_pcap_file("\t <-- Protocol: {}, Source: {}, Destination: {}".format(packet_protocol, packet_source, packet_destination))
-                    else: pass
-
-                    if packet_protocol == 1:
-                        (icmp_packet_type, icmp_packet_code, icmp_check_summation, icmp_packet_data) = pk.handle_icmp_packet(data)
-                        opt.__write_icmp_normal_output__(data)
                         if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("ICMP Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("ICMP Checksum: {}, ICMP Type: {}, ICMP Code: {}".format(icmp_check_summation, icmp_packet_type,icmp_packet_code))
+                            PCAP(pcap_file_name).write_to_pcap_file("\nIPv4 Packet Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
+                            PCAP(pcap_file_name).write_to_pcap_file("\t <-- Protocol: {}, Source: {}, Destination: {}".format(packet_protocol, packet_source, packet_destination))
                         else: pass
 
-                    elif packet_protocol == 6:
-                        (segment_source_port, segment_destination_port, segment_sequence, segment_acknowledgment, __urg_flag__, __ack_flag__,
-                        __psh_flag__, __rst_flag__, __syn_flag__, __fin_flag__) = pk.unpack_packet(cfg.ESPI_TCP_STRUCT_SEGMENT_FORMAT, data, 24)
+                        if packet_protocol == 1:
+                            (icmp_packet_type, icmp_packet_code, icmp_check_summation, icmp_packet_data) = pk.handle_icmp_packet(data)
+                            opt.__write_icmp_normal_output__(data)
+                            if args.filename:
+                                PCAP(pcap_file_name).write_to_pcap_file("ICMP Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("ICMP Checksum: {}, ICMP Type: {}, ICMP Code: {}".format(icmp_check_summation, icmp_packet_type,icmp_packet_code))
+                            else: pass
 
-                        so.__write_tcp_segment_normal_output__(raw_data)
+                        elif packet_protocol == 6:
+                            (segment_source_port, segment_destination_port, segment_sequence, segment_acknowledgment, __urg_flag__, __ack_flag__,
+                            __psh_flag__, __rst_flag__, __syn_flag__, __fin_flag__) = pk.unpack_packet(cfg.ESPI_TCP_STRUCT_SEGMENT_FORMAT, data, 24)
 
-                        if len(data) > cfg.ESPI_SUCCESS_CODE_STANDARD and args.httpraw:
-                            if segment_source_port == cfg.ESPI_HTTP_DEFAULT_PORT or segment_destination_port == cfg.ESPI_HTTP_DEFAULT_PORT:
-                                pk.handle_raw_http_packet(data)
-                            else:
-                                esp.print_espionage_noprefix('\t\t' + "Raw TCP/Raw-no-http Packet Bytes: ")
-                                print(espionage_textwrapper('\t\t\t', data))
-                        else: pass
-                        if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\tTCP Segment {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port # {}, Destination Port # {}, [Sequence] {}".format(segment_source_port,
-                            segment_destination_port, segment_sequence))
-                        else: pass
-                    elif packet_protocol == 17:
-                        (segment_source_port, segment_destination_port, segment_length, segdata) = seg.load_udp_segment(data)
-                        so.__write_udp_segment_normal_verbose_output__(segdata)
-                        if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\tUDP Segment (len={}) {}".format(segment_length, cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port: {}, Target Port: {}".format(segment_source_port, segment_destination_port))
-                        else: pass
+                            so.__write_tcp_segment_normal_output__(raw_data)
 
+                            if len(data) > cfg.ESPI_SUCCESS_CODE_STANDARD and args.httpraw:
+                                if segment_source_port == cfg.ESPI_HTTP_DEFAULT_PORT or segment_destination_port == cfg.ESPI_HTTP_DEFAULT_PORT:
+                                    pk.handle_raw_http_packet(data)
+                                else:
+                                    esp.print_espionage_noprefix('\t\t' + "Raw TCP/Raw-no-http Packet Bytes: ")
+                                    print(espionage_textwrapper('\t\t\t', data))
+                            else: pass
+                            if args.filename:
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\tTCP Segment {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port # {}, Destination Port # {}, [Sequence] {}".format(segment_source_port,
+                                segment_destination_port, segment_sequence))
+                            else: pass
+                        elif packet_protocol == 17:
+                            (segment_source_port, segment_destination_port, segment_length, segdata) = seg.load_udp_segment(data)
+                            so.__write_udp_segment_normal_verbose_output__(segdata)
+                            if args.filename:
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\tUDP Segment (len={}) {}".format(segment_length, cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port: {}, Target Port: {}".format(segment_source_port, segment_destination_port))
+                            else: pass
+                except: pass
         except KeyboardInterrupt:
             if args.filename:
                 print(BOLD + R + "\nExiting Espionage Interception.\n" + BOLD + G + "Packet capture saved to: {}".format(os.path.realpath(pcap_file_name)) + END)
@@ -206,56 +206,56 @@ def espionage_main():
                 print(BOLD + G + "[espionage]>" + W + BOLD + 'Ethernet Frame: ')
                 esp.print_espionage_notab('Destination: {}, Source: {}, Protocol: {}\n'.format(dest_mac, src_mac, eth_proto))
 
-                (packet_version, packet_header_length, packet_ttl, packet_protocol, packet_source, packet_destination, pkdata) = pk.handle_ipv4_packet(data)
+                try:
+                    (packet_version, packet_header_length, packet_ttl, packet_protocol, packet_source, packet_destination, pkdata) = pk.handle_ipv4_packet(data)
 
-                if eth_proto == 8:
-                    opt.__write_ipv4_verbose_output__(data)
-                    if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("\nIPv4 Packet Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("\t <-- Protocol: {}, Source: {}, Destination: {}".format(packet_protocol, packet_source, packet_destination))
-                    else: pass
-
-                    if packet_protocol == 1:
-                        (icmp_packet_type, icmp_packet_code, icmp_check_summation, icmp_packet_data) = pk.handle_icmp_packet(data)
-                        opt.__write_icmp_verbose_output__(data)
+                    if eth_proto == 8:
+                        opt.__write_ipv4_verbose_output__(data)
                         if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("ICMP Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("ICMP Checksum: {}, ICMP Type: {}, ICMP Code: {}".format(icmp_check_summation, icmp_packet_type,icmp_packet_code))
-                            PCAP(pcap_file_name).write_to_pcap_file("\t\tICMP Data: {}".format(icmp_packet_data))
+                                PCAP(pcap_file_name).write_to_pcap_file("\nIPv4 Packet Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("\t <-- Protocol: {}, Source: {}, Destination: {}".format(packet_protocol, packet_source, packet_destination))
                         else: pass
 
-                    elif packet_protocol == 6:
-                        (segment_source_port, segment_destination_port, segment_sequence, segment_acknowledgment, __urg_flag__, __ack_flag__,
-                        __psh_flag__, __rst_flag__, __syn_flag__, __fin_flag__) = pk.unpack_packet(cfg.ESPI_TCP_STRUCT_SEGMENT_FORMAT, data, 24)
-                        so.__write_tcp_segment_verbose_output__(raw_data)
+                        if packet_protocol == 1:
+                            (icmp_packet_type, icmp_packet_code, icmp_check_summation, icmp_packet_data) = pk.handle_icmp_packet(data)
+                            opt.__write_icmp_verbose_output__(data)
+                            if args.filename:
+                                PCAP(pcap_file_name).write_to_pcap_file("ICMP Contents {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("ICMP Checksum: {}, ICMP Type: {}, ICMP Code: {}".format(icmp_check_summation, icmp_packet_type,icmp_packet_code))
+                                PCAP(pcap_file_name).write_to_pcap_file("\t\tICMP Data: {}".format(icmp_packet_data))
+                            else: pass
 
-                        if len(data) > cfg.ESPI_SUCCESS_CODE_STANDARD and args.httpraw:
-                            if segment_source_port == cfg.ESPI_HTTP_DEFAULT_PORT or segment_destination_port == cfg.ESPI_HTTP_DEFAULT_PORT:
-                                pk.handle_raw_http_packet(data)
-                            else:
-                                esp.print_espionage_noprefix('\t\t' + "Raw TCP/Raw-no-http Packet Bytes: ")
-                                print(espionage_textwrapper('\t\t\t', raw_data))
-                        else: pass
+                        elif packet_protocol == 6:
+                            (segment_source_port, segment_destination_port, segment_sequence, segment_acknowledgment, __urg_flag__, __ack_flag__,
+                            __psh_flag__, __rst_flag__, __syn_flag__, __fin_flag__) = pk.unpack_packet(cfg.ESPI_TCP_STRUCT_SEGMENT_FORMAT, data, 24)
+                            so.__write_tcp_segment_verbose_output__(raw_data)
 
-                        if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\tTCP Segment {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port # {}, Destination Port # {}, [Sequence] {}".format(segment_source_port,
-                            segment_destination_port, segment_sequence))
-                            # Write TCP Segment flags to file
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\t\tTCP Segment Flags")
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\t\tFLAG_URG: {}, FLAG_ACK: {}, FLAG_PSH: {}, FLAG_RST: {}".format(__urg_flag__,
-                            __ack_flag__, __psh_flag__, __rst_flag__))
+                            if len(data) > cfg.ESPI_SUCCESS_CODE_STANDARD and args.httpraw:
+                                if segment_source_port == cfg.ESPI_HTTP_DEFAULT_PORT or segment_destination_port == cfg.ESPI_HTTP_DEFAULT_PORT:
+                                    pk.handle_raw_http_packet(data)
+                                else:
+                                    esp.print_espionage_noprefix('\t\t' + "Raw TCP/Raw-no-http Packet Bytes: ")
+                                    print(espionage_textwrapper('\t\t\t', raw_data))
+                            else: pass
 
-                        else: pass
+                            if args.filename:
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\tTCP Segment {}".format(cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port # {}, Destination Port # {}, [Sequence] {}".format(segment_source_port,
+                                segment_destination_port, segment_sequence))
+                                # Write TCP Segment flags to file
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\t\tTCP Segment Flags")
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\t\tFLAG_URG: {}, FLAG_ACK: {}, FLAG_PSH: {}, FLAG_RST: {}".format(__urg_flag__,
+                                __ack_flag__, __psh_flag__, __rst_flag__))
 
-                    elif packet_protocol == 17:
-                        (segment_source_port, segment_destination_port, segment_length, segdata) = seg.load_udp_segment(data)
-                        so.__write_udp_segment_normal_verbose_output__(segdata)
-                        if args.filename:
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\tUDP Segment (len={}) {}".format(segment_length, cfg.ESPI_ASCII_DOWN_ARROW))
-                            PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port: {}, Target Port: {}".format(segment_source_port, segment_destination_port))
-                        else: pass
-
+                            else: pass
+                        elif packet_protocol == 17:
+                            (segment_source_port, segment_destination_port, segment_length, segdata) = seg.load_udp_segment(data)
+                            so.__write_udp_segment_normal_verbose_output__(segdata)
+                            if args.filename:
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\tUDP Segment (len={}) {}".format(segment_length, cfg.ESPI_ASCII_DOWN_ARROW))
+                                PCAP(pcap_file_name).write_to_pcap_file("\n\t\tSource Port: {}, Target Port: {}".format(segment_source_port, segment_destination_port))
+                            else: pass
+                except: pass
 
         except KeyboardInterrupt:
             if args.filename:
